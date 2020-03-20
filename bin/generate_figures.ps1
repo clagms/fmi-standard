9114ad226ed72515999f9c130d93275a8c733b9e
Thomas Beutlich <modelica@tbeu.de>
2020-03-20 12:04:44 +0100

    Get required executable

--- bin/generate_figures.ps1
+++ bin/generate_figures.ps1
@@ -1,19 +1,26 @@
 
 function Export-Schema() {
-	Param(
-		$element, 
-		$expand, 
-		$preview = $false
-	)
-	$out = $element + "_schema.png"
-	$generator = "C:\srcctrl\xsddiagram\XSDDiagramConsole\bin\Debug\XSDDiagramConsole.exe"
-	& $generator -o .\docs\images\gen\$out -r $element -e $expand -d -c -z 300 -a -no-gui -y .\schema\fmi3ModelDescription.xsd
-	if ($preview) {
-		Start-Process .\docs\images\gen\$out
-	}
+    Param(
+        $element, 
+        $expand, 
+        $preview = $false
+    )
+    $out = $element + "_schema.png"
+    $generator = ".\XSDDiagramConsole.exe"
+    & $generator -o .\docs\images\gen\$out -r $element -e $expand -d -c -z 300 -a -no-gui -y .\schema\fmi3ModelDescription.xsd
+    if ($preview) {
+        Start-Process .\docs\images\gen\$out
+    }
 }
 
 $preview = $false
+
+if (-Not (Test-Path ".\XSDDiagramConsole.exe"))
+{
+    Invoke-WebRequest -Uri "http://regis.cosnier.free.fr/soft/xsddiagram/XSDDiagram-2018-06-19-1.2-Binary.zip" -OutFile "XSDDiagram-Binary.zip"
+    Expand-Archive -Path XSDDiagram-Binary.zip -DestinationPath . -Force
+    Remove-Item -Path XSDDiagram-Binary.zip
+}
 
 Export-Schema fmiModelDescription 1 $preview
 Export-Schema TypeDefinitions 2 $preview 
